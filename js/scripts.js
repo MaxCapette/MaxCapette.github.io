b49fc6b7bc4ab715c5e9707111c9d26ccf996d4d
Dokuyaku <sk8baz@gmail.com>
2023-07-29 17:36:40 +0200

    ajout 2 projets

--- js/scripts.js
+++ js/scripts.js
@@ -69,6 +69,7 @@ const theme = {
         theme.body = document.querySelector("body");
         theme.main = document.querySelector("main");
         theme.about = document.querySelector(".about");
+        
      
         
   
@@ -76,6 +77,7 @@ const theme = {
         theme.body.classList.toggle("theme-dark");
         theme.main.classList.toggle("theme-dark");
         theme.about.classList.toggle("theme-dark");
+    
     }
   };
   
